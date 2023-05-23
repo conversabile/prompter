16bc716b05af383322c9959536dafd767304c22e
dariowho <dario.chi@inventati.org>
2023-05-23 17:40:15 +0200

    Rename svelte folder to 'frontend'

--- frontend/src/routes/default/+page.svelte
+++ frontend/src/routes/default/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-	import Counter from './Counter.svelte';
+	import Counter from '../Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Conversabile Prompter</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
